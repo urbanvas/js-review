d2d55c0b36ecd24c4ab38ec6293a1d98b302a1f2
Saeed Jabbar <saeedryanjabbar@gmail.com>
2017-07-25 23:40:13 -0400

    push

---
+++ README.md
@@ -0,0 +1,15 @@
+# Vanilla refresher
+
+## What is this?
+
+This portion of the homework is a list of JavaScript tasks that touch on some fundamental JavaScript concepts.
+
+## Why are we doing it?
+
+Since we've been digging in so deeply into some complex topics, we want to take a step back and refresh some of the fundamentals outside the context of the more challenging stuff (like Express). This isn't a test and will not be graded or anything. The main purpose that it serves is for us to try to pin-point particular areas that we can have review sessions for. Since we have project week next week, we'll set aside an hour here and there for optional review sessions on some topics. The TAs may also do the same if we want to focus on particular things for longer periods of time.
+
+## How do we do it?
+
+We want you to do these tasks **by yourself** and **closed book**. The goal is not to test anyone. We really want to know what we should focus on for the review sessions. Are you completely stuck on classes? That's fine, we'll review it. I want to get an accurate account of what we need to review. We'll likely have mini, optional reviews for most of these topics so don't feel like you **should** know any particular task.
+
+When you try some of the DOM tasks, feel free to use DOM documentation, the MDN site or W3 schools.
---
+++ fundamentals.js
@@ -0,0 +1,67 @@
+// Create a variable `unicorn` and assign it to a string "STAR"
+
+
+
+// Declare an array containing all months in a year as strings, and console.log() "June"
+
+
+
+// Create a for loop that loops through all months in the months array and console.log()s each month
+
+
+
+// Declare a function `currentMonth` that accepts an array as a parameter, loops through the array, and returns "April".
+
+
+
+// Call the `currentMonth` function and pass the array created above (with the months in it) as an argument.
+
+
+
+// Declare an object literal `student` with the following properties: firstName, lastName, schoolName, grade.
+// And methods to: `sayHello`, that would return a string `Hello!`
+
+
+
+// console.log() the first and last names using string interpolation (template literals)
+
+
+
+// invoke the sayHello() method of the student object
+
+
+
+// Declare a JS class `developer` that would accept `name` and `languages`(array) as arguments and assign it to corresponding properties.
+// additionally, every developer should have `coolnessLevel` property that would be initially set to 10.
+// Our `developer` class should have the following methods:
+// `sayName` - would return developer's name
+// `status` - would return all data about our developer.
+// `learnLanguage` - would accept a language as an argument and push it to the array of languages. aaaaand increase `coolnessLevel` by 1.
+
+
+
+// create 2 instances of the `developer` class.
+
+
+// call `sayName` method 
+
+// call `status` method
+
+// call `learnLanguage` method and pass 'javascript' to it.
+
+
+
+/**
+ * FOR THIS SECTION YOU CAN USE THE MDN OR W3 SCHOOL DOCUMENTATION ABOUT THE DOM
+ */
+
+// create a paragraph element with a class of "vince" and the text content "hello world!". Then, append it to the element of the DOM with id `blue-cat`.
+
+
+
+// Create an array of numbers 1-5
+// Loop through the array, create a new <p> element for each number with the text content of the number, assign it to a variable, and append it to the body of the DOM.
+
+// find a div with id "parrot" and assign it to a variable.
+
+
